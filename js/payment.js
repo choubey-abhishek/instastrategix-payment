c5037184bb9ce1193464140e0dccd6c94d8da0b8
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-02-12 14:32:24 +0530

    Update payment.js

--- js/payment.js
+++ js/payment.js
@@ -1,1 +1,29 @@
+const stripe = Stripe("pk_test_YOUR_PUBLISHABLE_KEY");
+
+async function startPayment(packageType) {
+
+    try {
+        const response = await fetch("http://localhost:4242/create-checkout-session", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ packageType })
+        });
+
+        const session = await response.json();
+
+        if (session.id) {
+            stripe.redirectToCheckout({
+                sessionId: session.id
+            });
+        } else {
+            alert("Payment error. Try again.");
+        }
+
+    } catch (error) {
+        console.error("Payment error:", error);
+        alert("Server error. Please try later.");
+    }
+}
 
